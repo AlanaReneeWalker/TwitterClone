932fc52786982c70cab0e2e538c4a3726777ce9a
Maxim Stoupnikov <taomax8@gmail.com>
2015-10-08 22:16:25 -0400

    Form signin/signup work

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require "sinatra"
 require "sinatra/activerecord"
 require "bundler/setup"
+require 'rack-flash' 
 require "./models"
 
 set :database, "sqlite3:DogCrimes.sqlite3"
-
-
 enable :sessions
+set :sessions, true
+use Rack::Flash, sweep: true
 
 
 
@@ -28,11 +29,11 @@ end
 post "/signup" do
   @user = User.create(fname: params[:fname], 
   	 				  lname: params[:lname], 
-  	 				  email: param[:email],
-  	 				  username: param[:username],
-  	 				  password: param[:password])
+  	 				  email: params[:email],
+  	 				  username: params[:username],
+  	 				  password: params[:password])
   session[:user_id] = @user.id
-  redirect to "/users/:user_id"
+  redirect "/users/:user_id"
 end
 
 get "/signin" do
@@ -41,11 +42,13 @@ end
 
 
 post "/signin" do
+	@user=User.where(username: params[:username]).last
   if @user && @user.password == params[:password]
     session[:user_id] = @user.id
     flash[:notice] = "You've been signed in successfully."  
-    redirect to "/users/:user_id" 
-	else     
-	flash[:alert] = "There was a problem signing you in."
+    redirect "/users/#{@user.id}" 
+  else     
+	flash[:alert] = "Wrong username and/or password."
+	redirect "/signin" 
   end
 end
