930358a77f1957285db139a0d64aa80ec685d81d
Maxim Stoupnikov <taomax8@gmail.com>
2015-10-12 23:48:25 -0400

    Profile/Account page fix

--- app.rb
+++ app.rb
@@ -11,29 +11,28 @@ enable :sessions
 set :sessions, true
 use Rack::Flash, sweep: true
 
+
 def current_user
-	if session [:user_id]
+	if session[:user_id]
 		@current_user = User.find(session[:user_id])
 	end
 end
 
+
 get "/" do
+ # if session[:user_id]
   @posts=Post.all
+  if session[:user_id]
+  	flash[:info] = "You've been signed in successfully."
+  else
+  flash[:alert] = "You need to log in  or sign up to see a full website."
+  end
   erb :index
+  # else
+  # flash[:alert] = "You need to log in."
+  # redirect "/signin"
+ # end
 end
-
-get "/users/:users.id/posts" do
-	@posts = Post.where(user_id: session[:user_id])
-	erb :posts
-end
-
-get "/users/:user.id/connections" do
-	@usersFollowee= current_user.followees
-	@usersFollowers= current_user.followers
-	erb :connections
-end
-
-
 
 
 get "/signup" do
@@ -48,7 +47,7 @@ post "/signup" do
   	 				  username: params[:username],
   	 				  password: params[:password])
   flash[:info] = "Please sign in using your new username and password."
-  redirect "/sign_in"
+  redirect "/signin"
 end
 
 
@@ -70,6 +69,46 @@ post "/signin" do
 end
 
 
+get "/logout" do
+	 if session[:user_id]
+		@user = User.find(session[:user_id])
+		session[:user_id] = nil
+		redirect '/'
+	 else
+	 	redirect '/'
+	 end
+end
+
+
+
+
+get "/users/:users.id/posts" do
+	@posts = Post.where(user_id: session[:user_id])
+	erb :posts
+end
+
+get "/users/:user.id/connections" do
+	@usersFollowee= current_user.followees
+	@usersFollowers= current_user.followers
+	erb :connections
+end
+
+get "/users/:followee_id/follow" do
+  Follow.create(follower_id: session[:user_id], followee_id: params[:followee_id])
+  redirect "/users/:users.id/posts"
+end
+
+get "/users/:followee_id/unfollow" do
+  @follow = Follow.where(follower_id: session[:user_id], followee_id: params[:followee_id]).first
+  @follow.destroy
+  redirect "/users/:users.id/posts"
+end
+
+
+
+
+
+
 post "/make-post" do
 	@post = Post.create(subject: params[:subject],
 						body: params[:body],
@@ -79,12 +118,11 @@ post "/make-post" do
 end
 
 
-get "/logout" do
-	 if session[:user_id]
-		@user = User.find(session[:user_id])
-		session[:user_id] = nil
-		redirect '/'
-	 else
-	 	redirect '/'
-	 end
+
+
+post '/delete' do
+	@user = User.where(username: params[:username]).last
+	User.delete
+	session[:user_id] = nil
+	redirect '/'
 end
